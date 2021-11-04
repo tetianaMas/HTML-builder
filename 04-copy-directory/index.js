27652ba4c1b28b25882ba1462ec7179e5b8aee53
Tetiana Maslova <tetiana.maslova.qa@gmail.com>
2021-11-05 00:46:22 +0100

    feat: add 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,40 @@
+const fs = require('fs').promises;
+const path = require('path');
+const pathToFiles = path.join(__dirname, 'files');
+const pathToCopyFiles = path.join(__dirname, 'files-copy');
 
+async function copyDir() {
+  try {
+    await fs.mkdir(pathToCopyFiles, { recursive: true });
+    const copyFiles = await fs.readdir(pathToCopyFiles);
+    removeFiles(copyFiles);
+    const files = await fs.readdir(pathToFiles);
+
+    files.forEach(async file => {
+      try {
+        await fs.copyFile(
+          path.resolve(pathToFiles, file),
+          path.resolve(pathToCopyFiles, file)
+        );
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function removeFiles(files) {
+  if (files.length) {
+    files.forEach(async file => {
+      try {
+        await fs.unlink(path.resolve(pathToCopyFiles, file));
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  }
+}
+
+copyDir();
