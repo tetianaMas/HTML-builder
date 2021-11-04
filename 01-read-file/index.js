a667f68213ed7ac963adf6bd34ad93ca21eb5f78
Tetiana Maslova <tetiana.maslova.qa@gmail.com>
2021-11-04 11:53:33 +0100

    refactor: change event on reading data

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -6,9 +6,8 @@ async function readFile() {
   try {
     const file = await fs.open(path.join(__dirname, 'text.txt'));
     const stream = file.createReadStream({ encoding: 'utf-8' });
-    stream.on('readable', () => {
-      stream.close();
-      process.stdout.write(stream.read());
+    stream.on('data', data => {
+      process.stdout.write(data.toString());
     });
   } catch (err) {
     console.log(err);
